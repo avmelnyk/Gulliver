a6d0526d0b5488d0d669df3103553cdf4efa03f9
Yaroslav <nilfalse@yandex.com>
2018-09-20 18:55:48 +0300

    request product data

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,75 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+import { get } from './ajax';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+interface IProduct {
+  product_id: number;
+  category: string;
+  code: string;
+  name: string;
+  price: number;
 }
 
-export default App;
+interface IAppState {
+  isLoading: boolean | null;
+  product: IProduct | null;
+}
+
+export class App extends React.Component {
+  state: IAppState = {
+    isLoading: null,
+    product: null,
+  }
+
+  componentDidMount () {
+    this.setState({
+      isLoading: true
+    }, async () => {
+      this.setState({
+        isLoading: false,
+        product: await this._fetchProduct()
+      });
+    });
+  }
+
+  render () {
+    const { isLoading, product } = this.state;
+
+    if (isLoading) {
+      return (
+        <div className="app">
+          ... loading ...
+        </div>
+      );
+    }
+
+    if (product) {
+      return (
+        <div className="app">
+          <header className="product__header">
+            <h6>{product.category}</h6>
+            <h1 className="product__title">{product.name}</h1>
+            <small>код: {product.code}</small>
+            <p>{product.price / 100} грн</p>
+          </header>
+          <p className="product__description">
+            hello, the description is going to be here
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="app">
+          <header className="product__header">
+            <h1 className="product__title">404 &mdash; No Product Found</h1>
+          </header>
+        </div>
+      );
+    }
+  }
+
+  private _fetchProduct(): Promise<IProduct> {
+    return get('/api');
+  }
+}
