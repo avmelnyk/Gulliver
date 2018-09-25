dc8ca04eb191afd7b494710b31771d9b52a0cd02
Yaroslav <nilfalse@yandex.com>
2018-09-25 16:39:36 +0300

    add POST & PATCH ajax methods

--- web/src/ajax.tsx
+++ web/src/ajax.tsx
@@ -1,3 +1,25 @@
 export function get (url: string) {
   return fetch(url).then(response => response.json());
 }
+
+export function post (url: string, data: any) {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json());
+}
+
+export function patch (url: string) {
+  return fetch(url, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json());
+}
